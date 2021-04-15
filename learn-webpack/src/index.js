86a4a8b392a2c19efbfd98586f3ca7fd9b9b6551
West Wong <westwong.0622@gmail.com>
2021-04-15 18:00:49 +0800

    import toml, yaml, json5

--- learn-webpack/src/index.js
+++ learn-webpack/src/index.js
@@ -3,6 +3,18 @@ import "./index.css";
 import Icon from "./icon.png";
 import Data from "./data/data.xml";
 import Notes from "./data/data.csv";
+import toml from "./data/data.toml";
+import yaml from "./data/data.yaml";
+import json from "./data/data.json5";
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement("div");
