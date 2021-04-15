c7a6a324a084343c864a7feab28c1b3b4f12975a
West Wong <westwong.0622@gmail.com>
2021-04-15 18:03:23 +0800

    clean

--- learn-webpack/src/index.js
+++ learn-webpack/src/index.js
@@ -1,32 +1,8 @@
 import _ from "lodash";
-import "./index.css";
-import Icon from "./icon.png";
-import Data from "./data/data.xml";
-import Notes from "./data/data.csv";
-import toml from "./data/data.toml";
-import yaml from "./data/data.yaml";
-import json from "./data/data.json5";
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
