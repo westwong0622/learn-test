6008593c1f9e0bb96e33a2d20872b0b16649bf34
West Wong <westwong.0622@gmail.com>
2021-04-15 20:11:19 +0800

    add mode: "development", devtool: "inline-source-map",

--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -2,10 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  devtool: "inline-source-map",
   plugins: [new HtmlWebpackPlugin({})],
   output: {
     path: path.resolve(__dirname, "dist"),
