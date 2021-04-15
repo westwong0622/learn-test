4525b3c6768bda7499ee756ce4f0f8dc26af7764
West Wong <westwong.0622@gmail.com>
2021-04-15 18:09:29 +0800

    build to two bundle.js

--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
   },
 };
