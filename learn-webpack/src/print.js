a797c8ac98931875069b869e6240703e1330dfc4
West Wong <westwong.0622@gmail.com>
2021-04-15 23:32:17 +0800

    remove

--- learn-webpack/src/print.js
+++
@@ -1,7 +0,0 @@
-import _ from "lodash";
-
-console.log(_.join(["Another", "module", "loaded!"], " "));
-
-export default function printMe() {
-  console.log("I get called from print.js!");
-}
--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -4,15 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    index: {
-      import: "./src/index.js",
-      dependOn: "shared",
-    },
-    print: {
-      import: "./src/print.js",
-      dependOn: "shared",
-    },
-    shared: "lodash",
+    index: "./src/index.js",
   },
   devtool: "inline-source-map",
   devServer: {
