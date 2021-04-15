0bd996efb0ec74dca4111b5c9fc22523e1454eb7
West Wong <westwong.0622@gmail.com>
2021-04-15 16:46:48 +0800

    try import icon.png

--- learn-webpack/src/index.js
+++ learn-webpack/src/index.js
@@ -1,10 +1,15 @@
 import _ from "lodash";
 import "./index.css";
+import Icon from "./icon.png";
 
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
