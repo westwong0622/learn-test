6066c47e478acdd2138e29809febbd27de7037fa
West Wong <westwong.0622@gmail.com>
2021-04-15 18:13:36 +0800

    add new HtmlWebpackPlugin({})

--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  plugins: [new HtmlWebpackPlugin({})],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
