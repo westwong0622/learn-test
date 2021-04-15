daf77d8c9ed0a7ee94272500e000799c2c0dd729
West Wong <westwong.0622@gmail.com>
2021-04-15 16:41:52 +0800

    try style-loader

--- learn-webpack/src/index.js
+++ learn-webpack/src/index.js
@@ -1,8 +1,10 @@
 import _ from "lodash";
+import "./index.css";
 
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
   return element;
 }
