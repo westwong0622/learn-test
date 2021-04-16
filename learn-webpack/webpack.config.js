2055b423be12b5eb553b09777f7592ff37365177
West Wong <westwong.0622@gmail.com>
2021-04-16 23:31:57 +0800

    splitChunks > vendor > test: /[\\/]node_modules[\\/]/

--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -18,5 +18,14 @@ module.exports = {
   },
   optimization: {
     runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
 };
