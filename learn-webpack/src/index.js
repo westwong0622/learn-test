6f766a2fa23a0b43c2f07e5843db8b96e57b19dc
West Wong <westwong.0622@gmail.com>
2021-04-18 13:07:11 +0800

    re-render component

--- learn-webpack/src/index.js
+++ learn-webpack/src/index.js
@@ -10,11 +10,14 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
 
 if (module.hot) {
   module.hot.accept("./print.js", function () {
     console.log("Accepting the updated printMe module!");
-    Print();
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
   });
 }
