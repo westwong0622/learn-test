2ffa81f1cbfcbf8c0a8f707ad1e05f9e0b884824
West Wong <westwong.0622@gmail.com>
2021-04-19 00:17:36 +0800

    test

--- learn-webpack/src/index.js
+++ learn-webpack/src/index.js
@@ -1,24 +1,16 @@
 import _ from "lodash";
-import Print from "./print";
+import { cube } from "./math.js";
 import "./index.css";
 
-function component() {
-  const element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.onclick = Print;
+function component() {
+  const element = document.createElement("pre");
+
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
+    "\n\n"
+  );
 
   return element;
 }
 
-let element = component();
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept("./print.js", function () {
-    console.log("Accepting the updated printMe module!");
-    document.body.removeChild(element);
-    element = component();
-    document.body.appendChild(element);
-  });
-}
+document.body.appendChild(component());
