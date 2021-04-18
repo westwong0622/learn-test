af1ad4fa1ae7ddf03ee70fd4039ca9382f67927e
West Wong <westwong.0622@gmail.com>
2021-04-18 12:58:09 +0800

    recover webpack.config.js

--- learn-webpack/src/index.js
+++ learn-webpack/src/index.js
@@ -5,9 +5,16 @@ function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.onclick = Print.bind(null, "Hello webpack!");
+  element.onclick = Print;
 
   return element;
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept("./print.js", function () {
+    console.log("Accepting the updated printMe module!");
+    Print();
+  });
+}
